bff3661f6ce823076f4c0e4bec89985e80719324
Jamal Hamilton <hamilton.jamal.anthony@gmail.com>
2021-06-11 10:52:56 -0400

    initial

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # React-Django-eCommerce
 React Django practice ecommerce site
+
+#python > 3.8
+#Node > 16
---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Row, Col } from 'react-bootstrap';
+import Product from '../components/Product';
+import Loader from '../components/Loader';
+import Message from '../components/Message'; 
+import { listProducts } from '../actions/productActions';
+import Paginate from '../components/Paginate';
+import ProductCarousel from '../components/ProductCarousel'
+
+const HomeScreen = ({ history }) => {
+    const dispatch = useDispatch();
+    const productList = useSelector(state => state.productList);
+    const { error, loading, products, page, pages } = productList;
+
+    let keyword = history.location.search
+    console.log(keyword)
+
+    useEffect(() => {
+        dispatch(listProducts(keyword))
+    }, [dispatch, keyword])
+
+    return (
+        <div>
+            <ProductCarousel />
+            <h1>Latest Products</h1>
+            {loading ? <Loader />
+                :error ? <Message variant='danger'>{error}</Message>
+                :
+                <div>
+                <Row>
+                    {products.map(product => (
+                        <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
+                            <Product product={product} />
+                        </Col>
+                    ))}
+                </Row>
+                <Paginate page={page} pages={pages} keyword={keyword} />
+                </div>
+            }
+            
+        </div>
+    )
+}
+
+export default HomeScreen
